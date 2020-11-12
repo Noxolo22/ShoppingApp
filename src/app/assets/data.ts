add9b5a148d12a88fdbed97df3e845bdce651f3e
Noxolo22 <sinoxolonoxolo77@gmail.com>
2020-11-12 13:19:31 +0200

    first commit

---
+++ src/app/assets/data.ts
@@ -0,0 +1,51 @@
+export const data = [
+  {
+    "id": "1",
+    "title": "DELL",
+    "description": "The highest-resolution 15 laptop on the planet features an optional Quad HD+ (3200x1800) touch display that is 5x the resolution of a standard HD display. Our most powerful 15-inch 2-in-1. Built with the first-ever Intel® Core™ processor with Radeon™ Vega M discrete graphics on a single chip.",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRC-w5NM7Lf_wWKlJSu5YblqvdBy4xyF8RRWQ&usqp=CAU",
+    "price" : "16 000"
+  },
+  {
+    "id": "2",
+    "title": "HP",
+    "description": "Microprocessor. Intel® Core™ i5-4210U with Intel HD Graphics 4400 (1.7 GHz, up to 2.7 GHz, 3 MB cache, 2 cores",
+    "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQwErIPbViEzLVoE4Y1F_Zxl_Lr0FNVvk7xag&usqp=CAU",
+    "price" : "13 000"
+  },
+  {
+    "id": "3",
+    "title": "Acer ",
+    "description": "General. Brand. Acer. ModelDisplay. Size. 15.60-inch. Resolution. ..Processor. Processor. Intel Core i7 7th Gen 7100U. ...Memory. RAM. 8GB.Graphics. Graphics Processor. Intel HD Graphics 620. ...Storage. Hard disk. 1TB. ...Connectivity. Wi-Fi standards supported. 802.11 ac.Inputs. Finger Print Sensor. No.",
+    "image": "https://images.idgesg.net/images/article/2019/10/aceraspire1-100815704-large.jpg",
+    "price" : "59 000"
+  },
+  {
+    "id": "4",
+    "title": "Dell",
+    "description": "The highest-resolution 15 laptop on the planet features an optional Quad HD+ (3200x1800) touch display that is 5x the resolution of a standard HD display. Our most powerful 15-inch 2-in-1. Built with the first-ever Intel® Core™ processor with Radeon™ Vega M discrete graphics on a single chip.",
+    "image": "https://www.intel.com/content/dam/products/catalog/global/images/1x1/laptops-2in1-1x1.png.rendition.intel.web.550.550.png",
+    "price" : "16 000"
+  },
+  {
+    "id": "5",
+    "title": "Asus Vivobook",
+    "description": "Processor. Intel® Pentium® Quad-Core N3700 Processor, ...perating System.Memory. DDR3L 1600 MHz SDRAM, OnBoard Memory 2 GB / 4 GB, /8 GB.Display. 15.6 16:9 HD (1366x768)/Full HD (1920x1080)Graphic. Integrated Storage500GBHDD 5400 RPM.Optical Drive. ...Card Reader.",
+    "image": "https://www.makro.co.za/sys-master/images/ha7/ha1/9391276064798/silo-MIN_382362_EAC_large",
+    "price" : "10 000"
+  },
+  {
+    "id": "6",
+    "title": "HP",
+    "description": "Microprocessor. Intel® Core™ i5-4210U with Intel HD Graphics 4400 (1.7 GHz, up to 2.7 GHz, 3 MB cache, 2 cores",
+    "image": "https://m.economictimes.com/thumb/msid-67344076,width-1200,height-900,resizemode-4,imgsize-288701/hp-envy.jpg",
+    "price" : "13 000"
+  },
+  {
+    "id": "7",
+    "title": "Macbook pro",
+    "description": "Finish. Silver Space Grey.Display. Retina display. ...Processor. 1.4GHz 8th-generation quad‑core Intel Core i5, Turbo Boost up to 3.9GHz, with 128MB of eDRAM. ...Storage1 256GB. ...Memory. 8GB. ...Graphics. Intel Iris Plus Graphics 645. ...Charging and Expansion. ...Keyboard and Trackpad",
+    "image": "https://d2um6umu4gwssp.cloudfront.net/apple/wp-content/uploads/2019/01/macbook-pro-15-1-15-inch-mid-2018-feature.png",
+    "price" : "34 000"
+  },
+];
